4ce73c22af5b8076bf9526f90d6730af88da6fc4
Aayush Singh <98281650+singh-aayush@users.noreply.github.com>
2025-08-16 00:46:00 +0530

    bug fixing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3535",
+      "/api": "https://youtube-clone-erlr.onrender.com", // For local host we was using http://localhost:3535 path
     },
   },
   plugins: [react()],
