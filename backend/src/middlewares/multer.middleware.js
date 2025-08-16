a20756d03e6ab859f5ece2ce0dd115724c9d6b25
Aayush Singh <98281650+singh-aayush@users.noreply.github.com>
2025-08-16 20:10:49 +0530

    bug fixing

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,4 +1,12 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+const tempDir = path.join(process.cwd(), "public/temp");
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true });
+}
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
